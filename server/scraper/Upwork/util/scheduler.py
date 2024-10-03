de89f46d6560f0cb3aae784ac9e5798789ec2b98
Pranav Kolte <pranavkolte111@gmail.com>
2024-10-03 20:12:05 +0530

    updated data extract issue

--- server/scraper/Upwork/util/scheduler.py
+++ server/scraper/Upwork/util/scheduler.py
@@ -2,11 +2,11 @@ from server.scraper.Upwork.util.link import extract_links
 from server.scraper.Upwork.scraper import fetch_page_content, extract_job_info
 
 
-def job_data_extract(url):
+def job_data_extract(url: str) -> None:
 
-    url_list: list = extract_links(url)
+    url_list: list[str] = extract_links(url)
     for url in url_list:
-        print(f"Job Info for URL: {url}")
+        print(f"fetching data for: {url}")
         page_content = fetch_page_content(url)
         job_info = extract_job_info(page_content, url)
         print(job_info)
